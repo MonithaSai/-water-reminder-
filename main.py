649d9cf1f0782af9620fb5c077f141c10c70f38a
MonithaSai <124250119+MonithaSai@users.noreply.github.com>
2023-07-15 09:25:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+from plyer import notification
+
+if __name__=="__main__":
+      while True:
+          notification.notify(
+            title = "please drink water now !!!",
+            message = "get hydrated....cheer up !!",
+            app_icon = r"C:\Users\DELL\Desktop\ppppppppp\icon.ico.ico",
+            timeout = 5
+          )
+          time.sleep(60*60)
+   
+           
+       
+    
+    
